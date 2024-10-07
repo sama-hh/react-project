894e333ffde0effeab03a1f12d3727b88991eed4
Pavlo Bystrytskyi <pavlo.bystrytskyi@gmail.com>
2024-10-07 15:06:50 +0200

    Basic task done.

---
+++ src/Blog.tsx
@@ -0,0 +1,10 @@
+import Navigation from "./Blog/Navigation.tsx";
+import Body from "./Blog/Body.tsx";
+
+export default function Blog() {
+    return <div>
+        <h1>Blog</h1>
+        <Navigation/>
+        <Body/>
+    </div>
+}
---
+++ src/Blog/Navigation.tsx
@@ -0,0 +1,24 @@
+export default function Navigation() {
+    return <div className="navigation">
+        <h2>Navigation</h2>
+        <nav className="navbar">
+            <ul className="nav-ul">
+                <li className="nav-li">
+                    <a href="#first-entry">First entry</a>
+                </li>
+                <li className="nav-li">
+                    <a href="#second-entry">Second entry</a>
+                </li>
+                <li className="nav-li">
+                    <a href="#third-entry">Third entry</a>
+                </li>
+                <li className="nav-li">
+                    <a href="#fourth-entry">Fourth entry</a>
+                </li>
+                <li className="nav-li">
+                    <a href="#fifth-entry">Fifth entry</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+}
